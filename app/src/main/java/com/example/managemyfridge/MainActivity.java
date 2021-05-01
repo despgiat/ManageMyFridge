6633ed7cfdb5589370d4e1bd99d48e497db237ad
Despoina Yatagana <despoinayatagana@gmail.com>
2021-05-01 19:09:04 +0300

    Made the screen scrollable in case of landscape mode.

--- app/src/main/java/com/example/managemyfridge/MainActivity.java
+++ app/src/main/java/com/example/managemyfridge/MainActivity.java
@@ -3,12 +3,58 @@ package com.example.managemyfridge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView itemShowName;
+    EditText itemName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        itemShowName = findViewById(R.id.textView2);
+        itemName = findViewById(R.id.editTextTextPersonName);
+
+        if (savedInstanceState != null) {
+            //Retrieve data from the Bundle (other methods include getInt(), getBoolean() etc)
+            CharSequence userText = savedInstanceState.getCharSequence("savedUserText");
+            CharSequence displayText = savedInstanceState.getCharSequence("savedDisplayText");
+            //Restore the dynamic state of the UI
+            itemName.setText(userText);
+            itemShowName.setText(displayText);
+        } else {
+            //Initialize the UI
+            itemName.setText("");
+            itemShowName.setHint("Name");
+            itemShowName.setText("TextView");
+
+
+
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //Save data to the Bundle (other methods include putInt(), putBoolean() etc)
+        CharSequence userText = itemName.getText();
+        outState.putCharSequence("savedUserText", userText);
+        CharSequence displayText = itemShowName.getText();
+        outState.putCharSequence("savedDisplayText", displayText);
+    }
+
+    public void addNewItem(View view)
+    {
+        itemShowName.setText("You have added " + itemName.getText().toString());
+    }
+
+    public void itemOpened(View view)
+    {
+
     }
 }
