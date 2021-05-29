a560e426aad8f1c83aee2270e7a9f4d7a5f55410
alexandrakitsa <alexandrak@csd.auth.gr>
2021-05-29 19:43:04 +0100

    aaaa

--- app/src/main/java/com/example/managemyfridge/LoginScreen.java
+++ app/src/main/java/com/example/managemyfridge/LoginScreen.java
@@ -3,12 +3,76 @@ package com.example.managemyfridge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginScreen extends AppCompatActivity {
 
+    EditText username;
+    EditText password;
+    Button register;
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+        setupUI();
+        setupListeners();
+    }
+
+    private void setupListeners() {
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkUsername();
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent i = new Intent(LoginActivity.this, RegistrationActivity.class);
+                //startActivity(i);
+            }
+        });
+    }
+
+    private void checkUsername() {
+        boolean isValid = true;
+        boolean isemail = false;
+        if (isEmpty(username)) {
+            username.setError("Please enter an username or email address to login");
+            isValid = false;
+        }
+        if (isEmpty(password)) {
+            password.setError("Please enter the password to login");
+            isValid = false;
+        }
+        if (isValid) {
+            String usernameValue = username.getText().toString();
+            String passwordValue = password.getText().toString();
+            if (usernameValue.equals("0")) {//we need to check from db
+                //move to next screen
+            } else {
+                Toast t = Toast.makeText(this, "Wrong Username or password", Toast.LENGTH_SHORT);
+                t.show();
+            }
+        }
+
+    }
+    boolean isEmpty(EditText text) {
+        CharSequence str = text.getText().toString();
+        return TextUtils.isEmpty(str);
+    }
+    private void setupUI() {
+        username = findViewById(R.id.editTextTextEmailAddress);
+        password = findViewById(R.id.editTextTextPassword);
+        register = findViewById(R.id.button3);
+        login = findViewById(R.id.button2);
     }
 }
--- app/src/main/java/com/example/managemyfridge/SignUpScreen.java
+++ app/src/main/java/com/example/managemyfridge/SignUpScreen.java
@@ -3,12 +3,55 @@ package com.example.managemyfridge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignUpScreen extends AppCompatActivity {
+    EditText username;
+    EditText confirmpassword;
+    EditText password;
+    EditText email;
+    Button register;
+    Button login;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_screen);
+        username = findViewById(R.id.editTextTextPersonName2);
+        password = findViewById(R.id.editTextTextPassword6);
+        confirmpassword = findViewById(R.id.editTextTextPassword7);
+        email = findViewById(R.id.editTextTextEmailAddress5);
+        register = findViewById(R.id.button11);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkDataEntered();
+            }
+        });
+
+    }
+
+    private void checkDataEntered() {
+        if (isEmpty(username)) {
+            Toast t = Toast.makeText(this, "You must enter first name to register!", Toast.LENGTH_SHORT);
+            t.show();
+        }
+        if (!isEmail(email)) {
+            email.setError("Enter valid email!");
+        }
+    }
+    boolean isEmail(EditText text) {
+        CharSequence email = text.getText().toString();
+        return (!TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
+    }
+    boolean isEmpty(EditText text) {
+        CharSequence str = text.getText().toString();
+        return TextUtils.isEmpty(str);
     }
 }
